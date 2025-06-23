bfc6782efc9cafa60f939d7164ed519b4b05bf5b
jknott1 <jknott@luc.edu>
2025-06-23 22:35:12 +0000

    Added overload add() method

--- DoubleLinkedList.java
+++ DoubleLinkedList.java
@@ -5,12 +5,14 @@ public class DoubleLinkedList implements Comparable<nfs_DoubleLinkedList> {
     private String name;
     private Node head;
     private Node tail;
+    private int size; // ASSIGNMENT 04 - added new parameter that keeps count of nodes.
 
     /** Basic constructor */
     public DoubleLinkedList(String name) {
         this.name = name;
         this.head = null;
         this.tail = null;
+        this.size = 0 // ASSIGNMENT 04 - sets default value to 0, as no nodes exist in the linked list yet. 
     } // Basic constructor
 
     /** Default constructor passes default name to basic constructor */
@@ -31,14 +33,27 @@ public class DoubleLinkedList implements Comparable<nfs_DoubleLinkedList> {
             this.head = node;
         } else {
             this.tail.setNext(node);
+            node.setPrevious(this.tail); // ASSIGNMENT 04 - sets each 
         }
         this.tail = node;
-        node.setPrevious(this)
+        this.size++; // This increases the size variable by 1 whenever a node is added to the list
     } // method add
 
     // overload method add to add a node by value
     public void add(String value) {
+        Node node1 = new Node(); // creates a new Node object
+        node1.value = value; // the value of the new node 
+        if (this.head == null) {
+            this.head = node1;
+        } else {
+            this.tail.setNext(node1);
+            node1.setPrevious(this.tail); // sets new node's previous cursor to the old node. 
+        }
+        this.tail = node1;
+        this.size++; 
     } // method add
+
+
 
     // implement the comparable interface
     public int compareTo(nfs_DoubleLinkedList other) {
